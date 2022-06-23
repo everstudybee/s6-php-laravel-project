c8d8150db5b8f08447981c915da8837bf468580c
everstudybee <everstudybee@gmail.com>
2022-06-23 22:36:53 +0200

    feat: add all migrations for breeze

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix
-    .js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ])
-    .browserSync('127.0.0.1:8000')
-    .sourceMaps();
+mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
+    require('tailwindcss'),
+    require('autoprefixer'),
+]);
